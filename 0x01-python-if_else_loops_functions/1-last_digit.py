8a1860078b0e95023eaf259af3fc969a2bb5ebf4
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-09 14:27:22 +0000

    fixed checker type error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_dig = int(str((number))[-1])
+last_dig = int(str(number)[-1])
 if number < 0:
     last_dig *= -1
-print("Last digit of {0} is {1}".format(str(number), str(last_dig)), end=" ")
+print("Last digit of {0:d} is {1:d}".format(number, last_dig), end=" ")
 if last_dig == 0:
     print("and is 0")
 elif last_dig > 5:
