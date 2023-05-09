1cacbaad51c7b09b002176ff0f231eb6cad3f40a
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-09 14:03:26 +0000

    Attemptinf to fix checker type error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,11 @@ number = random.randint(-10000, 10000)
 last_dig = int(str((number))[-1])
 if number < 0:
     last_dig *= -1
-print("Last digit of {0} is {1}".format(number, last_dig), end=" ")
 if last_dig == 0:
-    print("and is 0")
+    print("Last digit of {0} is {1} and is 0".format(number, last_dig))
 elif last_dig > 5:
-    print("and is greater than 5")
+    print("Last digit of {0} is {1} and is\
+ greater than 5".format(number, last_dig))
 else:
-    print("and is less than 6 and not 0")
+    print("Last digit of {0} is {1} and is\
+ less than 6 and not 0".format(number, last_dig))
