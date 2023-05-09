d0c3e69d5f1053b2743bdce751089a68fbbbad9d
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-09 14:16:23 +0000

    changed vartype from int to str

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,10 @@ number = random.randint(-10000, 10000)
 last_dig = int(str((number))[-1])
 if number < 0:
     last_dig *= -1
+print("Last digit of {0} is {1}".format(str(number), str(last_dig)), end=" ")
 if last_dig == 0:
-    print("Last digit of {0} is {1} and is 0".format(number, last_dig))
+    print("and is 0")
 elif last_dig > 5:
-    print("Last digit of {0} is {1} and is\
- greater than 5".format(number, last_dig))
+    print("and is greater than 5")
 else:
-    print("Last digit of {0} is {1} and is\
- less than 6 and not 0".format(number, last_dig))
+    print("and is less than 6 and not 0")
