4fc53237ff43fcba65d31d69df138987c95be21d
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-12 23:08:54 +0000

    Task 14 - CPython list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - prints some basic info about Python lists
- * @p: pointer to PyObject
+ * @p: pointer to PyObject / list object
  *
  * Return: void
  */
 void print_python_list_info(PyObject *p)
 {
-
+	Py_ssize_t size = PyList_Size(p);
+	/*Retrieves size of python list*/
+	Py_ssize_t = i;
+	/*Declare variable i of type Py_ssize*/
+	printf("[*] Size of Python LIst = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	/*print size of memery alocated*/
+	for (i = 0; i < size; i++)
+	/*loop over items in python list*/
+	{
+		PyObject *item = PyList_GetItem(p, i);
+		/*items in list retrived*/
+		const char *typeName = Py_TYPE(item)->tp_name;
+		/*name of item*/
+		printf("Element %ld: %s\n", i, typeName);
+		/*print the index and type name*/
+	}
 }
