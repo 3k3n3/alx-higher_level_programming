ae197e2d3d69f672e9cf50a9e802d305d6afa5f8
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-15 20:37:45 +0000

    Fixed linting error on Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
     for line in matrix:
         for i in line:
-            print("{:d}{}".format(i, " " if l.index(i) != len(l) - 1
+            print("{:d}{}".format(i, " " if line.index(i) != len(line) - 1
                                   else ""), end="")
         print("")
