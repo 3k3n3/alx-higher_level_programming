fcc51fe6655e0bb08b296bdd81524a392201042b
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-17 21:35:47 +0000

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """Convert a roman numeral to an integer."""
+    roman_string = roman_string.upper()
+    numerals = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
+            }
+    integer = 0
+    for enum, x in enumerate(roman_string):
+        if enum < len(roman_string) - 1:
+            if numerals[roman_string[enum + 1]] > numerals[x]:
+                integer -= numerals[x]
+            else:
+                integer += numerals[x]
+        else:
+            integer += numerals[x]
+    return integer
