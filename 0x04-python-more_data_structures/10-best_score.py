8f507370b78d9d089a63c786c9a5729daa688c15
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-17 19:54:28 +0000

    Renamed files for Task 2 & 4, Correction for Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,3 +5,4 @@ def best_score(a_dictionary):
     if a_dict is not None:
         max_value = sorted(list(a_dict.values()))[-1]
         return "".join({i for i in a_dict if a_dict[i] == max_value})
+    return None
