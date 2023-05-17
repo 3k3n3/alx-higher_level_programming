a89d458e8b8809b551342e1342717d1379734882
3k3n3@vagrant <clarencekene@outlook.com>
2023-05-17 20:02:18 +0000

    Correction 2 for Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
     a_dict = a_dictionary
     if a_dict is not None:
+        if len(a_dict) == 0:
+            return None
         max_value = sorted(list(a_dict.values()))[-1]
         return "".join({i for i in a_dict if a_dict[i] == max_value})
     return None
