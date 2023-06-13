5a2f6fb4ed67c9d414767f0503ed77ca6f4895cf
3k3n3@vagrant <clarencekene@outlook.com>
2023-06-13 19:31:43 +0000

    Tasks 8 - 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3
+"""A class Student that defines a student."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiante method."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dict representation of student."""
+        if attrs is not None:
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of Student."""
+        for attr, value in json.items():
+            setattr(self, attr, value)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,2 +1,5 @@
 # 0x0B. Python - Input/Output
 
+## Additional Resource
+- [Using getattr, setattr, delattr, hasattr in Python](https://medium.com/@pranaygore/using-getattr-setattr-delattr-hasattr-in-python-6d79c6f9fda3)
+
