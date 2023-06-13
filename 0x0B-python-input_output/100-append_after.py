cc1c33ea0274626d4d21cec2340c59e596217046
3k3n3@vagrant <clarencekene@outlook.com>
2023-06-13 22:37:16 +0000

    Advanced 1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A function that inserts a line of text to a file,
+after each line containing a specific string."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file."""
+    text = ""
+    with open(filename) as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+
+    with open(filename, "w") as f:
+        f.write(text)
