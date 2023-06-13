bd518036e1b1267f287ed019153d73a42e6f48d0
3k3n3@vagrant <clarencekene@outlook.com>
2023-06-13 22:31:15 +0000

    Pascal's Triangle 2

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,6 +6,9 @@ def pascal_triangle(n):
     """Returns a list of lists of integers
     representing the Pascal's triangle of n."""
 
+    if n <= 0:
+        return []
+
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
