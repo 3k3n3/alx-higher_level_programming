7d66107dd32cd99d0fbe6f195502e8aaf8398876
3k3n3@vagrant <clarencekene@outlook.com>
2023-06-13 16:09:29 +0000

    Re- Tasks 0 - 7, README

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that reads a text file (UTF8) and prints it to stdout."""
+
+
+def read_file(filename=""):
+    """Reads and prints a text file."""
+    with open(filename) as f:
+        print(f.read())
