74dd399903dc1ce9abaa8bbde42f4084b99675b9
3k3n3@vagrant <clarencekene@outlook.com>
2023-06-13 16:10:47 +0000

    Reset

--- 0x08-python-more_classes/1-write_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A function that writes a string to a text file (UTF8)
-and returns the number of characters."""
-
-
-def write_file(filename="", text=""):
-    """Write to file and return num of chars."""
-    with open(filename, "w") as f:
-        return f.write(text)
