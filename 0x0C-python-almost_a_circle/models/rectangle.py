0a8cf7498f17f3822c47d1ddfa1ad37b1cdcbc26
3k3n3 <80990247+3k3n3@users.noreply.github.com>
2023-06-22 13:29:24 +0100

    update getter and setter property

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -53,7 +53,7 @@ class Rectangle(Base):
         """Public setter for width."""
         if (type(width) is not int):
             raise TypeError("width must be an integer")
-        elif width <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
@@ -67,7 +67,7 @@ class Rectangle(Base):
         """Public setter for height."""
         if (type(height) is not int):
             raise TypeError("height must be an integer")
-        elif height <= 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
 
